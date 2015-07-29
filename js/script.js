0d391518011c22f2b05a5549b6b79beea506f71e
Soudabeh Sheikhi <soudabeh.sheikhi@gmail.com>
2015-07-28 23:02:20 -0700

    added function to search every word  and checking that input is not empty before showing the list

--- js/script.js
+++ js/script.js
@@ -8,38 +8,38 @@
 			$('#credit-card').empty();
 			var $input = $('#input').val();
 			$input = $input.toLocaleLowerCase();
-			$.getJSON('products.json',
-			 function(products){
-		        	$.each(products, function(index, product){
-		        		$.each(product, function(index, item){
-		        			$dataItem = (item.name).toLocaleLowerCase();
-		        			if ($dataItem.startsWith($input)){
-		        				if (item.type == 'BANK'){
-         							$('#bank').append('<li class="bankName">' + item.name + ' </li>');
-         							$('#bank').append('<li class="url"><a href="' + item.url + '"</a>' + item.url + '</li>');
-		        				};
-		        				if(item.type == 'LOAN'){
-         							$('#loan').append('<li class="bankName">' + item.name + ' </li>');
-         							$('#loan').append('<li class="url"><a href="' + item.url + '"</a>' + item.url + '</li>');
-		        				};
-		        				if(item.type == 'INVESTMENT'){
-         							$('#investment').append('<li class="bankName">' + item.name + ' </li>');
-         							$('#investment').append('<li class="url"> <a href="' + item.url + '"</a>' + item.url + '</li>');
-		        				};
+			if($input){
+				$.getJSON('products.json',function(products){
+			        $.each(products, function(index, product){
+			        	$.each(product, function(index, item){
+			        		$dataItem = (item.name).toLocaleLowerCase();
+			        		if ($dataItem.indexOf($input) > 0){
+			        			if (item.type == 'BANK'){
+	         						$('#bank').append('<li class="bankName">' + item.name + ' </li>');
+	         						$('#bank').append('<li class="url"><a href="' + item.url + '"</a>' + item.url + '</li>');
+			        			};
+			        			if(item.type == 'LOAN'){
+	         						$('#loan').append('<li class="bankName">' + item.name + ' </li>');
+	         						$('#loan').append('<li class="url"><a href="' + item.url + '"</a>' + item.url + '</li>');
+			        			};
+			        			if(item.type == 'INVESTMENT'){
+	         						$('#investment').append('<li class="bankName">' + item.name + ' </li>');
+	         						$('#investment').append('<li class="url"> <a href="' + item.url + '"</a>' + item.url + '</li>');
+			        			};
 
-		        				if(item.type == 'MORTGAGE'){
-         							$('#mortgage').append('<li class="bankName">' + item.name + ' </li>');
-         							$('#mortgage').append('<li class="url"><a href="' + item.url + '"</a>' + item.url + '</li>');
-		        				};
-		        				if(item.type == 'CREDIT_CARD'){
-         							$('#credit-card').append('<li class="bankName">' + item.name + ' </li>');
-         							$('#credit-card').append('<li class="url"><a href="' + item.url + '"</a>' + item.url + '</li>');
-		        				};
-		        			};
-		        		});
-		        	});
-			})
-
+			        			if(item.type == 'MORTGAGE'){
+	         						$('#mortgage').append('<li class="bankName">' + item.name + ' </li>');
+	         						$('#mortgage').append('<li class="url"><a href="' + item.url + '"</a>' + item.url + '</li>');
+			        			};
+			        			if(item.type == 'CREDIT_CARD'){
+	         						$('#credit-card').append('<li class="bankName">' + item.name + ' </li>');
+	         						$('#credit-card').append('<li class="url"><a href="' + item.url + '"</a>' + item.url + '</li>');
+			        			};
+			        		};
+			        	});
+			        });
+				});
+			};
 		});
 	});
  
